9c493482463b2b358b6dca3acec33e3db6f458e9
icare-beep <icare@rbcalbayogcity.com>
2026-03-10 16:15:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, 'bank_pin_registration.html');
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.end('File not found');
-      return;
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(data);
-  });
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/bank_pin_registration.html');
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT);
